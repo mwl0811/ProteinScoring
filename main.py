3f0101b01264e99b470fb2f6757897b78eb202e8
mwl0811 <406594891@qq.com>
2022-03-04 14:48:41 +0000

    add the relative files

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 # This is a protein scoring program by data modeling.
 
-
+from Bio.PDB.PDBParser import PDBParser
 
 
 if __name__ == '__main__':
+    parser = PDBParser(PERMISSIVE=1)
 
+    structure_id = "test1"
+    filename = "1gsl_1.pdb"
+
+    structure = parser.get_structure(structure_id, filename)
+
+    # 获取头部信息
+    print(structure.header["resolution"])
+    # print(structure.header["keywords"])
